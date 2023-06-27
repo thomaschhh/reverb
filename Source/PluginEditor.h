1977d5225dfa81f75c7758d8b30c903112fd21e6
parednuw <mail@paulwunder.de>
2023-06-27 18:55:36 +0200

    Created generic GUI with Volume and Delaytime Slider as well as a Label.

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -14,7 +14,7 @@
 //==============================================================================
 /**
 */
-class ReverbAudioProcessorEditor  : public juce::AudioProcessorEditor
+class ReverbAudioProcessorEditor  : public juce::AudioProcessorEditor, public juce::Slider::Listener
 {
 public:
     ReverbAudioProcessorEditor (ReverbAudioProcessor&);
@@ -25,8 +25,13 @@ public:
     void resized() override;
 
 private:
-    // This reference is provided as a quick way for your editor to
-    // access the processor object that created it.
+    
+	void sliderValueChanged (juce::Slider* slider) override;
+	
+	juce::Slider mVolumeSlider;
+	juce::Slider mDelaytimeSlider;
+	juce::Label mReverbLabel;
+	
     ReverbAudioProcessor& audioProcessor;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (ReverbAudioProcessorEditor)
