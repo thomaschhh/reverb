a25600e62bbd9be600cbab50b7896a6274d0f75f
thomaschhh <holz.thomas@yahoo.de>
2024-01-03 18:47:48 +0100

    Remove comments

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -92,10 +92,7 @@ void ReverbAudioProcessor::changeProgramName (int index, const juce::String& new
 
 //==============================================================================
 void ReverbAudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlock)
-{
-    // Use this method as the place to do any pre-playback
-    // initialisation that you need..
-    
+{    
     int delayBufferSize = sampleRate * 2.0;
     delayBuffer.setSize(getTotalNumOutputChannels(), delayBufferSize);
 }
@@ -138,42 +135,23 @@ void ReverbAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, juce:
     auto totalNumInputChannels  = getTotalNumInputChannels();
     auto totalNumOutputChannels = getTotalNumOutputChannels();
 
-    // In case we have more outputs than inputs, this code clears any output
-    // channels that didn't contain input data, (because these aren't
-    // guaranteed to be empty - they may contain garbage).
-    // This is here to avoid people getting screaming feedback
-    // when they first compile a plugin, but obviously you don't need to keep
-    // this code if your algorithm always overwrites all the output channels.
     for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
         buffer.clear (i, 0, buffer.getNumSamples());
     
     int bufferSize = buffer.getNumSamples();
     int delayBufferSize = delayBuffer.getNumSamples();
     
-    // This is the place where you'd normally do the guts of your plugin's
-    // audio processing...
-    // Make sure to reset the state if your inner loop is processing
-    // the samples and the outer loop is handling the channels.
-    // Alternatively, you can process the samples with the channels
-    // interleaved by keeping the same state.
     for (int channel = 0; channel < totalNumInputChannels; ++channel)
     {
         auto* channelData = buffer.getWritePointer (channel);
         
-        // Copy input signal to a delay buffer
         fillDelayBuffer(channel, bufferSize, delayBufferSize, channelData);
-        
-        // Read from the past in the delay buffer, then add back to main buffer
         readFromDelayBuffer(buffer, delayBuffer, channel, bufferSize, delayBufferSize, channelData);
-        
-        // feeback
         fillDelayBuffer(channel, bufferSize, delayBufferSize, channelData);
-
     }
     
     writePositionDelayBuffer += bufferSize;
-    // ensure to stay in bounds of bufferSize, wrap around in circular buffer
-    writePositionDelayBuffer %= delayBufferSize;
+    writePositionDelayBuffer %= delayBufferSize;  // ensure to stay in bounds of bufferSize, wrap around in circular buffer
 }
 
 void ReverbAudioProcessor::readFromDelayBuffer(juce::AudioBuffer<float>& buffer, juce::AudioBuffer<float>& delayBuffer, int channel, int bufferSize, int delayBufferSize, float* channelData){
