9a1552ceb6cf5ab6bd3ba5983a839754dbb38b65
thomaschhh <holz.thomas@yahoo.de>
2024-01-03 19:46:16 +0100

    Fix cracking sound

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -162,13 +162,13 @@ void ReverbAudioProcessor::readFromDelayBuffer(juce::AudioBuffer<float>& buffer,
     }
     
     if (readPositionDelayBuffer + bufferSize < delayBufferSize) {
-        buffer.addFromWithRamp(channel, 0, delayBuffer.getReadPointer(channel, readPositionDelayBuffer), bufferSize, startGain, endGain);
+        buffer.addFromWithRamp(channel, 0, delayBuffer.getReadPointer(channel, readPositionDelayBuffer), bufferSize, endGain, endGain);
     }else{
         int numSamplesToEndOfDelayBuffer = delayBufferSize - readPositionDelayBuffer;
-        buffer.addFromWithRamp(channel, 0, delayBuffer.getReadPointer (channel, readPositionDelayBuffer), numSamplesToEndOfDelayBuffer, startGain, endGain);
+        buffer.addFromWithRamp(channel, 0, delayBuffer.getReadPointer (channel, readPositionDelayBuffer), numSamplesToEndOfDelayBuffer, endGain, endGain);
         
         auto numSamplesAtStartOfDelayBuffer = bufferSize - numSamplesToEndOfDelayBuffer;
-        buffer.addFromWithRamp(channel, numSamplesToEndOfDelayBuffer, delayBuffer.getReadPointer (channel, 0), numSamplesAtStartOfDelayBuffer, startGain, endGain);
+        buffer.addFromWithRamp(channel, numSamplesToEndOfDelayBuffer, delayBuffer.getReadPointer (channel, 0), numSamplesAtStartOfDelayBuffer, endGain, endGain);
     }
 }
 
@@ -177,15 +177,15 @@ void ReverbAudioProcessor::fillDelayBuffer(int channel, int bufferSize, int dela
     if (delayBufferSize > bufferSize + writePositionDelayBuffer){
         
         // copy main buffer contents to delay buffer
-        delayBuffer.copyFromWithRamp(channel, writePositionDelayBuffer, channelData, bufferSize, startGain, endGain);
+        delayBuffer.copyFromWithRamp(channel, writePositionDelayBuffer, channelData, bufferSize, startGain, startGain);
     }else{
         // copy remaining samples without wrapping around
         int numSamplesToEndOfDelayBuffer = delayBufferSize - writePositionDelayBuffer;
-        delayBuffer.copyFromWithRamp(channel, writePositionDelayBuffer, channelData, numSamplesToEndOfDelayBuffer, startGain, endGain);
+        delayBuffer.copyFromWithRamp(channel, writePositionDelayBuffer, channelData, numSamplesToEndOfDelayBuffer, startGain, startGain);
         
         // start from beginning again
         int numSamplesAtStartOfDelayBuffer = bufferSize - numSamplesToEndOfDelayBuffer;
-        delayBuffer.copyFromWithRamp(channel, 0, channelData + numSamplesToEndOfDelayBuffer, numSamplesAtStartOfDelayBuffer, startGain, endGain);
+        delayBuffer.copyFromWithRamp(channel, 0, channelData + numSamplesToEndOfDelayBuffer, numSamplesAtStartOfDelayBuffer, startGain, startGain);
         
     }
 }
